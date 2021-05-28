340e67a9d2907fdf69bfc5066400d282ef070525
Samuel Pascua <pascua.samuel.14@gmail.com>
2021-05-28 19:23:09 +0800

    block: cleanup realme changes
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>

--- block/elevator.c
+++ block/elevator.c
@@ -209,10 +209,6 @@ int elevator_init(struct request_queue *q, char *name)
 		return 0;
 
 	INIT_LIST_HEAD(&q->queue_head);
-#ifdef CONFIG_PRODUCT_REALME_TRINKET
-/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
-	INIT_LIST_HEAD(&q->fg_head);
-#endif /*CONFIG_PRODUCT_REALME_TRINKET*/
 	q->last_merge = NULL;
 	q->end_sector = 0;
 	q->boundary_rq = NULL;
@@ -424,10 +420,6 @@ void elv_dispatch_sort(struct request_queue *q, struct request *rq)
 	}
 
 	list_add(&rq->queuelist, entry);
-#ifdef CONFIG_PRODUCT_REALME_TRINKET
-/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
-	queue_throtl_add_request(q, rq, false);
-#endif
 }
 EXPORT_SYMBOL(elv_dispatch_sort);
 
@@ -448,10 +440,6 @@ void elv_dispatch_add_tail(struct request_queue *q, struct request *rq)
 	q->end_sector = rq_end_sector(rq);
 	q->boundary_rq = rq;
 	list_add_tail(&rq->queuelist, &q->queue_head);
-#ifdef CONFIG_PRODUCT_REALME_TRINKET
-/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
-	queue_throtl_add_request(q, rq, false);
-#endif
 }
 EXPORT_SYMBOL(elv_dispatch_add_tail);
 
@@ -685,20 +673,12 @@ void __elv_add_request(struct request_queue *q, struct request *rq, int where)
 	case ELEVATOR_INSERT_FRONT:
 		rq->rq_flags |= RQF_SOFTBARRIER;
 		list_add(&rq->queuelist, &q->queue_head);
-#ifdef CONFIG_PRODUCT_REALME_TRINKET
-/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
-		queue_throtl_add_request(q, rq, true);
-#endif /*CONFIG_PRODUCT_REALME_TRINKET*/
 		break;
 
 	case ELEVATOR_INSERT_BACK:
 		rq->rq_flags |= RQF_SOFTBARRIER;
 		elv_drain_elevator(q);
 		list_add_tail(&rq->queuelist, &q->queue_head);
-#ifdef CONFIG_PRODUCT_REALME_TRINKET
-/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
-		queue_throtl_add_request(q, rq, false);
-#endif
 		/*
 		 * We kick the queue here for the following reasons.
 		 * - The elevator might have returned NULL previously

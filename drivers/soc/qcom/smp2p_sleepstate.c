8c0372b2493d3fa686887e2d2053c0fddc692137
mcdofrenchfreis <xyzevan@androidist.net>
2022-11-15 16:53:05 +0800

    smp2p_sleepstate: fix compilation breakage
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>

--- drivers/soc/qcom/smp2p_sleepstate.c
+++ drivers/soc/qcom/smp2p_sleepstate.c
@@ -37,12 +37,12 @@ static int sleepstate_pm_notifier(struct notifier_block *nb,
 {
 	switch (event) {
 	case PM_SUSPEND_PREPARE:
-		qcom_smem_state_update_bits(state, AWAKE_BIT, 0);
+		qcom_smem_state_update_bits(qstate, AWAKE_BIT, 0);
 		usleep_range(10000, 10500); /* Tuned based on SMP2P latencies */
 		break;
 
 	case PM_POST_SUSPEND:
-		qcom_smem_state_update_bits(state, AWAKE_BIT, AWAKE_BIT);
+		qcom_smem_state_update_bits(qstate, AWAKE_BIT, AWAKE_BIT);
 		break;
 	}
 

aab956b43650f89da664a873a8e2a80e54680a02
John Galt <johngaltfirstrun@gmail.com>
2022-11-28 13:34:19 -0500

    Revert "mm: process reclaim: vmpressure based process reclaim"
    This reverts commit 7964b3ce47f0d87fbbb1cfdd1fb4aadb620133dd as QCOM vmpressure driven process reclaim is redundant compared to Linux PPR.
    
    Change-Id: Ib1407525dcce77b16679be32879bb26fe97139e8
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    [mcdofrenchfreis: drop more realme leftovers]
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2781,41 +2781,5 @@ static inline void setup_nr_node_ids(void) {}
 
 extern int want_old_faultaround_pte;
 
-#ifdef CONFIG_PROCESS_RECLAIM
-struct reclaim_param {
-	struct vm_area_struct *vma;
-	/* Number of pages scanned */
-	int nr_scanned;
-	/* max pages to reclaim */
-	int nr_to_reclaim;
-	/* pages reclaimed */
-	int nr_reclaimed;
-#ifdef CONFIG_PRODUCT_REALME_TRINKET
-	/* robin.ren@PSW.BSP.Kernel.Performance, 2019-03-13,
-	 * the target reclaimed process
-	 */
-	struct task_struct *reclaimed_task;
-#endif
-};
-extern struct reclaim_param reclaim_task_anon(struct task_struct *task,
-		int nr_to_reclaim);
-
-#ifdef CONFIG_PRODUCT_REALME_TRINKET
-/* Kui.Zhang@PSW.BSP.Kernel.Performance, 2019-01-01,
- * Extract the reclaim core code from task_mmu.c for /proc/process_reclaim*/
-extern ssize_t reclaim_task_write(struct task_struct* task,
-		char *buffer);
-
-#define PR_PASS		0
-#define PR_SEM_OUT	1
-#define PR_TASK_FG	2
-#define PR_TIME_OUT	3
-#define PR_ADDR_OVER	4
-#define PR_FULL		5
-#define PR_TASK_RUN	6
-#define PR_TASK_DIE	7
-#endif /* CONFIG_PRODUCT_REALME_TRINKET */
-#endif /* CONFIG_PROCESS_RECLAIM */
-
 #endif /* __KERNEL__ */
 #endif /* _LINUX_MM_H */

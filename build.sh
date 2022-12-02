a938b3f7f7b0592e4e026f593f89eaa59ed0e6ac
mcdofrenchfreis <xyzevan@androidist.net>
2022-12-02 10:17:03 +0800

    build.sh: update
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 # Copyright (C) 2022-2023 Mar Yvan D.
 set -uo pipefail
 
+# Cleanup first
+git clean -fdx
+git reset --hard
+
+# AK3 Dependency Cloning
 git clone --depth=1 https://github.com/mcdofrenchfreis/AnyKernel3.git -b r5x AnyKernel
 
 # Misc Stuff
@@ -12,46 +17,44 @@ export ARCH=arm64
 
 # Directory Variables
 KDIR=$(pwd)
-TCDIR=${KDIR}/clang
+TCDIR=/home/xevan/Development/Compiler/AOSP-clang
+GASDIR=/home/xevan/Development/Compiler/gas
 DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 
 # Kernel name/version Variables
 KNAME="Biofrost"
 MIN_HEAD=$(git rev-parse HEAD)
-export LOCALVERSION="-${KNAME}-$(cat version)"
-export ZIP_NAME="${KNAME}-$(cat version)"
+VERSION="$(cat version)/$(echo ${MIN_HEAD:0:8})"
+export LOCALVERSION="-${KNAME}-$(echo "${VERSION}")"
 
 # GitHub Variables
 export COMMIT_HASH=$(git rev-parse --short HEAD)
-export REPO_URL="https://github.com/mcdofrenchfreis/biofrost-kernel-realme-r5x"
-
-# Default device defconfig used.
-DEFCONFIG=biofrost_defconfig
+export REPO_URL="https://github.com/mcdofrenchfreis/biofrost-kernel-realme-trinket"
 
 # Build Information
+LINKER=ld.lld
 export COMPILER_NAME="$(${TCDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-export LINKER_NAME="$("${TCDIR}"/bin/ld.bfd --version | head -n 1 | sed 's/(compatible with [^)]*)//' | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-
+export LINKER_NAME="$("${TCDIR}"/bin/${LINKER} --version | head -n 1 | sed 's/(compatible with [^)]*)//' | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export KBUILD_BUILD_USER=xevan
-export KBUILD_BUILD_HOST=ArchLinux
-
+export KBUILD_BUILD_HOST=Androidist
 export DEVICE="Realme 5 Series"
 export CODENAME="realme_trinket"
+
+# Zipping Details
+export ZIP_NAME="${KNAME}-$(cat version)-$(echo ${MIN_HEAD:0:8}).zip"
 
 # Telegram Stuffs
 bot_token="bot5129489057:AAF5o-JfQ1iAUp9Min7Jcr9sHPjTpCaIlA8"
 chat_id="-1001736789494"
 
-# Post Main Information
 function sendinfo() {
     curl -s -X POST "https://api.telegram.org/$bot_token/sendMessage" \
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b> - Biofrost Laboratory | Machine Build Triggered</b>%0A<b>Maintainer: </b><code>${KBUILD_BUILD_USER} - ${KBUILD_BUILD_HOST}</code>%0A<b>Build Date: </b><code>$(date +"%Y-%m-%d %H:%M")</code>%0A<b>Device: </b><code>${DEVICE} (${CODENAME})</code>%0A<b>Kernel Version: </b><code>$(make kernelversion 2>/dev/null)</code>%0A<b>Compiler: </b><code>${COMPILER_NAME}</code>%0A<b>Linker: </b><code>${LINKER_NAME}</code>%0A<b>Zip Name: </b><code>${ZIP_NAME}</code>%0A<b>Branch: </b><code>$(git rev-parse --abbrev-ref HEAD)</code><code> (master)</code>%0A<b>Last Commit Details: </b><a href='${REPO_URL}/commit/${COMMIT_HASH}'>${COMMIT_HASH}</a> <code>($(git log --pretty=format:'%s' -1))</code>"
+        -d text="<b>Laboratory Machine: Build Triggered</b>%0A<b>Maintainer: </b><code>${KBUILD_BUILD_USER}</code>%0A<b>Build Date: </b><code>$(date +"%Y-%m-%d %H:%M")</code>%0A<b>Device: </b><code>${DEVICE} (${CODENAME})</code>%0A<b>Kernel Version: </b><code>$(make kernelversion 2>/dev/null)</code>%0A<b>Compiler: </b><code>${COMPILER_NAME}</code>%0A<b>Linker: </b><code>${LINKER_NAME}</code>%0A<b>Zip Name: </b><code>${ZIP_NAME}</code>%0A<b>Branch: </b><code>$(git rev-parse --abbrev-ref HEAD)</code><code> (master)</code>%0A<b>Last Commit Details: </b><a href='${REPO_URL}/commit/${COMMIT_HASH}'>${COMMIT_HASH}</a> <code>($(git log --pretty=format:'%s' -1))</code>"
 }   
-# Push Build to Channel
 function push() {
     cd AnyKernel
     ZIP=$(echo *.zip)
@@ -61,7 +64,6 @@ function push() {
         -F "parse_mode=html" \
         -F caption="Build took $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds. | <b>Compiled with: ${COMPILER_NAME} + ${LINKER_NAME}.</b>"
 }
-# Error? Press F
 function finerr() {
     curl -s -X POST "https://api.telegram.org/$bot_token/sendMessage" \
         -d chat_id="$chat_id" \
@@ -70,21 +72,13 @@ function finerr() {
         -d text="Compilation failed, please check build logs for errors."
     exit 1
 }
-# Compile >.<
 function compile() {
-    make O=out ARCH=arm64 ${DEFCONFIG}
-    make -j$(nproc --all) O=out \
-              PATH=${TCDIR}/bin/:${PATH} \
-              ARCH=arm64 \
-			  CC=clang \
-			  CROSS_COMPILE=aarch64-linux-gnu- \
-			  CROSS_COMPILE_ARM32=arm-linux-gnueabi- 
-		      AR=llvm-ar \
-              AS=llvm-as \
-			  OBJDUMP=llvm-objdump \
-			  STRIP=llvm-strip \
-			  NM=llvm-nm \
-			  OBJCOPY=llvm-objcopy \
+    make O=out ARCH=arm64 biofrost_defconfig
+    export PATH="${TCDIR}/bin:${GASDIR}:${PATH}"
+    export CROSS_COMPILE=aarch64-linux-gnu-
+    export CLANG_TRIPLE=aarch64-linux-gnu-
+    export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+    make -j$(nproc --all) O=out LLVM=1 LLVM-IAS=1 ARCH=arm64 \
 
     if ! [ -a "$IMAGE" ]; then
         finerr
@@ -101,7 +95,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 ${ZIP_NAME}.zip *
+    zip -r ${ZIP_NAME} . -x ".git*" -x "README.md" -x "*.zip"
     cd ..
 }
 sendinfo

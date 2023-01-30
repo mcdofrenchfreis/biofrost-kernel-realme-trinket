28593980487546520ed3fc697d365079aacab7e3
mcdofrenchfreis <xyzevan@androidist.net>
2023-01-30 10:20:04 +0800

    mm: Bump swappiness to 180
    ref: https://t.me/sultanskernel/413555
    
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -165,7 +165,7 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 /*
  * From 0 .. 200.  Higher means more swappy.
  */
-int vm_swappiness = 160;
+int vm_swappiness = 180;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
